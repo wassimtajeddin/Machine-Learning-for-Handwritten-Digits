c5c23000bcd283b2a09e4f57400cb7d84541b02f
wassimtajeddin <wassim.tajeddin@gmail.com>
2025-03-16 13:04:00 +0100

    Split dataset into training and testing sets

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import numpy as np
 from sklearn import datasets
+from sklearn.model_selection import train_test_split
 
 
 mnist = datasets.fetch_openml('mnist_784')
 X = np.array(mnist.data)
 y = np.array(mnist.target, dtype='int')
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
