848fade4444c3c5936a0bd2fb0c1170ab09bdfde
wassimtajeddin <wassim.tajeddin@gmail.com>
2025-03-17 17:40:28 +0100

    Train SVM model using the RBF kernel on training data

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
 
 
 mnist = datasets.fetch_openml('mnist_784')
@@ -8,3 +9,6 @@ X = np.array(mnist.data)
 y = np.array(mnist.target, dtype='int')
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = SVC(gamma='scale', kernel='rbf')
+model.fit(X_train, y_train)
