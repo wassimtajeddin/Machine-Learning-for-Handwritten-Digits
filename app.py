af627dcd5d7663773188df4bf2cdc6b5f68f8c56
wassimtajeddin <wassim.tajeddin@gmail.com>
2025-03-19 18:56:53 +0100

    Make predictions on the test dataset using the trained model

--- app.py
+++ app.py
@@ -12,3 +12,5 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 model = SVC(gamma='scale', kernel='rbf')
 model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
