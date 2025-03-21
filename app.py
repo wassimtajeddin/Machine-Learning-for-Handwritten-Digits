f13a900071acea6a79c2500e92b67fa8d493efab
wassimtajeddin <wassim.tajeddin@gmail.com>
2025-03-21 19:30:05 +0100

    Visualize test images along with true labels and predictions

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import numpy as np
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
+import matplotlib.pyplot as plt
 
 
 mnist = datasets.fetch_openml('mnist_784')
@@ -14,3 +15,14 @@ model = SVC(gamma='scale', kernel='rbf')
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
+
+def plot_digits(images, labels, predictions, n=10):
+    plt.figure(figsize=(10, 4))
+    for i in range(n):
+        plt.subplot(2, 5, i + 1)
+        plt.imshow(images[i].reshape(28, 28), cmap='gray')
+        plt.title(f'True: {labels[i]}\nPred: {predictions[i]}')
+        plt.axis('off')
+    plt.show()
+
+plot_digits(X_test, y_test, y_pred)
